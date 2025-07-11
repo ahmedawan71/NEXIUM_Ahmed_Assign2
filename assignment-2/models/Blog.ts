bbe0d809723193dc4cf43b5e897c34315f9c092f
Muhammad Ahmed <m.ahmedse05@gmail.com>
2025-07-12 01:20:26 +0500

    defined mongo schema and supabase table

---
+++ assignment-2/models/Blog.ts
@@ -0,0 +1,8 @@
+import mongoose from '../lib/mongo';
+
+const BlogSchema = new mongoose.Schema({
+  url: { type: String, required: true },
+  fullText: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now },
+});
+export const Blog = mongoose.models.Blog || mongoose.model('Blog', BlogSchema);
